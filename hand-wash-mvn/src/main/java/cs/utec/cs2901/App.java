44792fd32a323b70c8878b359f352e8146bb61af
GSR <39766479+fresh-avocado@users.noreply.github.com>
2020-04-01 13:37:22 -0500

    "lava tus manos" 100 times

---
+++ hand-wash-mvn/src/main/java/cs/utec/cs2901/App.java
@@ -0,0 +1,17 @@
+package cs.utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+		for (int i = 0; i < 100; i++)
+		{
+        	System.out.println( "Debo lavarme las manos por 20 segundos." );
+		}
+    }
+}
